6563bd3ce66102f10ddbafc6e29b7f489db2ca45
MelindaRespeto <145196890+MelindaRespeto@users.noreply.github.com>
2026-01-30 14:34:46 +0800

    add

--- chamber/scripts/thankyou.js
+++ chamber/scripts/thankyou.js
@@ -1,9 +1,8 @@
-
 const params = new URLSearchParams(window.location.search);
 
-document.getElementById("firstName").textContent = params.get("fname");
-document.getElementById("lastName").textContent = params.get("lname");
+document.getElementById("firstName").textContent = params.get("firstName");
+document.getElementById("lastName").textContent = params.get("lastName");
 document.getElementById("email").textContent = params.get("email");
-document.getElementById("mobile").textContent = params.get("mobile");
-document.getElementById("business").textContent = params.get("business");
+document.getElementById("mobile").textContent = params.get("phoneNumber");
+document.getElementById("business").textContent = params.get("orgName");
 document.getElementById("timestamp").textContent = params.get("timestamp");

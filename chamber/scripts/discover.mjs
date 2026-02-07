ac952b75c4dc3607f949d4058f1855d28f76b656
MelindaRespeto <145196890+MelindaRespeto@users.noreply.github.com>
2026-02-07 18:48:39 +0800

    add

--- chamber/scripts/discover.mjs
+++ chamber/scripts/discover.mjs
@@ -23,3 +23,24 @@ document.addEventListener("DOMContentLoaded", () => {
     cardsGrid.appendChild(card);
   });
 });
+const visitMessage = document.getElementById("visit-message");
+const lastVisit = localStorage.getItem("lastVisit");
+const now = Date.now();
+
+if (!lastVisit) {
+  visitMessage.textContent = "Welcome! Let us know if you have any questions.";
+} else {
+  const daysBetween = Math.floor((now - lastVisit) / (1000 * 60 * 60 * 24));
+
+  if (daysBetween < 1) {
+    visitMessage.textContent = "Back so soon! Awesome!";
+  } else if (daysBetween === 1) {
+    visitMessage.textContent = "You last visited 1 day ago.";
+  } else {
+    visitMessage.textContent = `You last visited ${daysBetween} days ago.`;
+  }
+}
+
+// Store the current visit time in milliseconds
+localStorage.setItem("lastVisit", now);
+

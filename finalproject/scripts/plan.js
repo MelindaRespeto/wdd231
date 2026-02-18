78dd552356d6b95496022ff7751006c1e4cecea8
MelindaRespeto <145196890+MelindaRespeto@users.noreply.github.com>
2026-02-18 13:32:43 +0800

    add

--- finalproject/scripts/plan.js
+++ finalproject/scripts/plan.js
@@ -94,3 +94,11 @@ getWeather();
 // ================= FOOTER INFO =================
 const dateEl = document.getElementById("date");
 if (dateEl) dateEl.textContent = document.lastModified;
+const hamburger = document.getElementById("hamburger");
+const navMenu = document.getElementById("nav-menu");
+
+if (hamburger && navMenu) {
+  hamburger.addEventListener("click", () => {
+    navMenu.classList.toggle("show");
+  });
+}
